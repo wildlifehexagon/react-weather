34748419ee69ef37bce07be8e7096be45bb635c1
wildlifehexagon <crossthebreeze@gmail.com>
2018-10-27 17:42:48 -0500

    refactor: remove unnecessary files, get flow ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { Component } from "react"
-import "./App.css"
+// @flow
+import React from "react"
 
-class App extends Component {
-  render() {
-    return <div>Weather App</div>
-  }
-}
+const App = () => <div>Weather App</div>
 
 export default App
